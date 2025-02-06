94ca2bfa2bc5c672decd7437c9081a307d69cfc4
Fae Burns <fae.burns0@gmail.com>
2025-02-06 12:16:37 +0000

    Initial Project Commit

---
+++ Crumpet.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Crumpet.Console;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        // IInterpreter interpreter = new Crumpet.Interpreter.CrumpetInterpreter();
+        // interpreter.Execute(File.ReadAllText(args[1]));
+    }
+}
